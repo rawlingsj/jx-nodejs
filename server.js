5ce967389aa1525cea7eafbf9a83a0c2f11efa06
James Strachan <james.strachan@gmail.com>
2019-11-14 14:28:07 +0000

    fix: return more than one file

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ var http = require('http');
 var fileSystem = require('fs');
 
 var server = http.createServer(function(req, resp){
-	fileSystem.readFile('./index.html', function(error, fileContent){
+	var fileName = './index.html';
+	var path = req.url;
+	if (path) {
+		if (path.endsWith(".css") || path.endsWith(".md") || path.endsWith(".svg") || path.endsWith(".ico")) {
+			fileName = "." + path;
+		}
+	}
+	console.log("request path ", path);	
+	fileSystem.readFile(fileName, function(error, fileContent){
 		if(error){
 			resp.writeHead(500, {'Content-Type': 'text/plain'});
 			resp.end('Error');
@@ -17,5 +25,4 @@ var server = http.createServer(function(req, resp){
 });
 
 server.listen(8080);
-
 
